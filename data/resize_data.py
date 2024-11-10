89a52255f6cd53aa9ca8cdf5adae5052e348826d
MathiasDamsgaard <mathias.c.damsgaard@gmail.com>
2024-11-10 11:12:27 +0100

    Get data from Kaggle and resize
    File to extract aircraft data from Kaggle and move it to the repo data folder. Also a script to resize the images to 224x224. Updated gitignore in data folder.

---
+++ data/resize_data.py
@@ -0,0 +1,42 @@
+import os
+from PIL import Image
+import pandas as pd
+
+
+# Define the source and destination directories
+source_dir = os.getcwd() + '/data'
+data_dir = os.path.join(source_dir, 'seryouxblaster764/fgvc-aircraft/versions/2/fgvc-aircraft-2013b/fgvc-aircraft-2013b/data/images')
+destination_dir = os.path.join(source_dir, 'resized_images')
+
+csv_dir = os.path.join(source_dir, 'seryouxblaster764/fgvc-aircraft/versions/2')
+csv_files = ['train.csv', 'val.csv', 'test.csv']
+output_csv = os.path.join(source_dir, 'data.csv')
+
+
+# Create the destination directory if it doesn't exist
+if not os.path.exists(destination_dir):
+    os.makedirs(destination_dir)
+
+# Define the new size
+new_size = (224, 224)
+
+# Loop through all files in the source directory
+for filename in os.listdir(data_dir):
+    if filename.endswith('.jpg'):
+        # Open an image file
+        with Image.open(os.path.join(data_dir, filename)) as img:
+            # Resize the image
+            resized_img = img.resize(new_size, Image.LANCZOS)
+            # Save it to the destination directory
+            resized_img.save(os.path.join(destination_dir, filename))
+
+print(f"All images have been resized to {new_size}, and saved to {destination_dir}")
+
+
+# Combine CSV files
+combined_df = pd.concat([pd.read_csv(os.path.join(csv_dir, f)) for f in csv_files], ignore_index=True)
+combined_df.reset_index(drop=True, inplace=True)  # Reset row indices
+
+# Save the combined DataFrame to a new CSV file
+combined_df.to_csv(output_csv, index=False)
+print(f"Combined CSV saved at: {output_csv}")
