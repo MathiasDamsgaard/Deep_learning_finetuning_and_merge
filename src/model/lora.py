ae6f4c7409cd1a9926319cb500a92fe7bdd79cba
Anton M. Storgaard <antonmosquerastorgaard@gmail.com>
2024-11-18 14:35:29 +0100

    added plus option

--- src/model/lora.py
+++ src/model/lora.py
@@ -1,4 +1,5 @@
 from peft import LoraConfig, get_peft_model
+from peft.optimizers import create_loraplus_optimizer
 from transformers import ViTImageProcessor, ViTModel, ViTForImageClassification
 import bitsandbytes as bnb
 from src.config.config import *
@@ -15,17 +16,28 @@ Q_lora_config = LoraConfig(init_lora_weights="gaussian", target_modules="all-lin
 
 def get_lora_config(type_: str) -> LoraConfig:
     if type_ == "lora":
-        return get_peft_model(base_model, lora_config)
-    
+        return get_peft_model(base_model, lora_config), None
+
     elif type_ == "Q_lora":
-        return get_peft_model(base_model, Q_lora_config)
-    
+        return get_peft_model(base_model, Q_lora_config), None
+
     elif type_ == "lora_plus":
-        raise NotImplementedError("LoRA+ is not implemented yet.")
-    
+        optimizer = create_loraplus_optimizer(
+            model=get_peft_model(base_model, lora_config),
+            optimizer_cls=bnb.optim.Adam8bit,
+            lr=5e-5,
+            loraplus_lr_ratio=16,
+        )
+        return get_peft_model(base_model, lora_config), optimizer
+
     elif type_ == "Q_lora_plus":
-        raise NotImplementedError("QLoRA+ is not implemented yet.")
-    
+        optimizer = create_loraplus_optimizer(
+            model=get_peft_model(base_model, Q_lora_config),
+            optimizer_cls=bnb.optim.Adam8bit,
+            lr=5e-5,
+            loraplus_lr_ratio=16,
+        )
+        return get_peft_model(base_model, Q_lora_config), optimizer
+
     else:
         raise ValueError(f"Invalid type: {type_}")
-
